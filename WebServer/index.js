518cd6032309337f970adb71738a0e80865689f9
Edivansanto5 <edivansantos1478@gmail.com>
2022-08-27 09:55:34 -0300

    criando o codico index

--- WebServer/index.js
+++ WebServer/index.js
@@ -1,1 +1,9 @@
-process.title = 'WebServer'
+process.title = 'WebServer';
+    let args = process.argv,
+        port = args[2] || 7070,
+        WebServer = Require('/.server');
+
+    WebServer.listen(port,function(){
+        console.log('Server start at port '+ port);
+    });
+
