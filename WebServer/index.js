e9eb690adfbd0b8d5f95ab6632585ec51e9f9e89
Edivansanto5 <edivansantos1478@gmail.com>
2022-08-28 09:22:26 -0300

    excluindo projetos

--- WebServer/index.js
+++
@@ -1,9 +0,0 @@
-process.title = 'WebServer';
-    let args = process.argv,
-        port = args[2] || 7070,
-        WebServer = require('/.server');
-
-    WebServer.listen(port,function(){
-        console.log('Server start at port '+ port);
-    });
-
